6fff94ed24657d25961c7d52e02433fec75b5b2f
108-0 <jk1028@g.shingu.ac.kr>
2025-04-07 15:50:52 +0900

    장르추천 완성

--- front/movie_js.js
+++ front/movie_js.js
@@ -12,9 +12,11 @@ let MovieObject = {
             console.log(response)
             movielist = response.result
 
-            topdiv = document.createElement("div")
-            topdiv.style = "column-count:5"
-            document.body.appendChild(topdiv)
+            // topdiv = document.createElement("div")
+            // topdiv.style = "column-count:5"
+            // document.body.appendChild(topdiv)
+
+            topdiv = document.getElementById("alldiv")
 
             movielist.forEach( movie => {
                 // 첫번째 영화이미지
@@ -38,7 +40,52 @@ let MovieObject = {
            // 실패코드
            console.log(error)
        });
-    }
+    },
+
+
+    getgenres: function(){
+        do_url = "http://localhost:8000/genresbest/" + document.getElementById("sgenre").value
+        // console.log(do_url)
+        $.ajax({
+        // 실행할 코드
+           type: "GET",
+           url: do_url,
+        }).done(function(response){
+        // 성공코드
+             console.log(response)
+             movielist = response.result
+            
+             topdiv = document.getElementById("genrediv")
+             
+             topdiv.innerHTML = "" 
+             
+            //  while(topdiv.firstChild){
+            //     topdiv.removeChild(topdiv.firstChild)  // 다른 방법
+            //  }
+             
+             movielist.forEach( movie => {
+                 // 첫번째 영화이미지
+                 cmovie = document.createElement("div")
+                 cmovie.className = "card"
+     
+                 mimg = document.createElement("img")
+                 mimg.className = "card-img-top"
+                 mimg.src = movie.poster_path
+                 mimg.onclick = function(){
+                 //    location.href = movie.url
+                     window.open(movie.url);
+                 }
+                 mimg.style.cursor = "pointer"
+                 cmovie.appendChild(mimg)
+                 topdiv.appendChild(cmovie)
+             });
+ 
+ 
+        }).fail(function(error){
+            // 실패코드
+            console.log(error)
+        });
+     }
 }
 
 MovieObject.init();
